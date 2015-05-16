297cbe796f4b7117b331bcee929b9f3bc1a9f618
Tim Peters <mail@darksecond.nl>
2015-05-16 18:51:55 +0200

    Remove hack

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,6 @@
 export CERTNAME=`hostname -f | sed 's/\./-/g'`
 cp /certs/$CERTNAME.key /etc/ssl/private/dovecot.key
 cp /certs/$CERTNAME.pem /etc/ssl/certs/dovecot.pem
-chmod 000 /certs # Make /certs inaccessible
-
-# (Temporary) Hack to make vmail work
-usermod -u `stat -c "%u" /var/vmail` vmail
-groupmod -g `stat -c "%g" /var/vmail` vmail
 
 chown -R vmail:vmail /var/vmail
 
